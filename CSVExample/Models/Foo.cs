9352a4a774fb52d372e530805c5c54f2dab49d2a
Tank.vn <tanvd357@gmail.com>
2023-07-25 11:21:08 +0700

    create a model class named Foo
    This class is used to read and write CSV files.

---
+++ CSVExample/Models/Foo.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CSVExample.Models
+{
+	public class Foo
+	{
+		[Name("id")]
+		public int Id { get; set; }
+
+		[Name("名")]
+		public string FirstName { get; set; }
+
+		[Name("姓")]
+		public string LastName { get; set; }
+
+		[Name("年齢")]
+		public int? Age { get; set; }
+
+		[Name("アクティブ")]
+		public string IsActive { get; set; }
+	}
+}
